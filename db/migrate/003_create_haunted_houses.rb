91377123891ebe05c70ca30ded0bbb66edbfac58
Brent Bauman <brentbauman86@gmail.com>
2018-07-18 22:15:52 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,14 @@
 class CreateHauntedHouse < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_house do |table|
-      table.
+      table.text :name
+      table.string :location
+      table.string :theme
+      table.integer :price
+      table.boolean :family_friendly
+      table.datetime :opening_date
+      table.datetime :closing_date
+      table.text :description
   end
 end
 end
